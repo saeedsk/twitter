315070e349a5e33ca042e6dec49954677bae07a6
Saeed <Saeed@Karimabadi.ca>
2017-02-02 02:57:16 -0500

    Complete tweets histogram over the course of 7 month

--- twitter.R
+++ twitter.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(bit64)
 
 load_dataset <- function() {
-  tweets <- data.table::fread("tweets.csv", na.strings="NA", colClasses=NULL, nrows=4000000)
+  tweets <- data.table::fread("tweets.csv", na.strings="NA", colClasses=NULL, nrows=4800000)
   tweets <- cbind(timestamp=as.POSIXct(paste (tweets$tweet_time, tweets$tweet_date, sep = " ") , format="%H:%M:%S %Y-%m-%d"), tweets)
   tweets <- cbind(date=as.POSIXct(tweets$tweet_date, format="%Y-%m-%d"), tweets)
   tweets$tweet_time <- NULL
@@ -14,7 +14,8 @@ load_dataset <- function() {
 }
 
 show_histogram <- function(tweets) {
-  hist(tweets$timestamp, length(tweets$timestamp), breaks = 300, freq = TRUE, xlab = "Date", ylab = " Tweets Frequency", main = " Tweets Histogram")
+  #number of tweets per hour
+  hist(tweets$timestamp, length(tweets$timestamp), breaks = 5040, freq = TRUE, xlab = "Date", ylab = " Tweets Frequency", main = " Tweets Histogram")
 }
 
 
