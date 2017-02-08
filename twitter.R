0a4455f725664391df9df9a1c9d642e89599806a
Saeed <Saeed@Karimabadi.ca>
2017-02-08 13:53:03 -0500

    initial nlp codes

--- twitter.R
+++ twitter.R
@@ -1,35 +1,79 @@
 
-packages <- c("ggplot2", "lubridate", "RTextTools", "bit64", "stringr", "date", "scales")
+packages <- c("ggplot2", "lubridate", "RTextTools", "bit64", "stringr", "date", "scales", "textcat", "NLP", "SnowballC")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages()))) 
 }
 
+library(textcat)
 library(lubridate)
 library(ggplot2)
 library(bit64)
 library(stringr)
 library(date)
 library(scales)
+library(tm)
+library(SnowballC)
 library("RTextTools") #Loads many packages useful for text mining
-
+#----------------------------------------------------------------
 printf <- function(...) cat(sprintf(...))
-
+#----------------------------------------------------------------
 load_dataset <- function() {
 #  tweets <- data.table::fread("tweets.csv", na.strings="NA", colClasses=NULL, nrows=4800000)
-  tweets <- data.table::fread("tweets.csv", na.strings="NA", colClasses=NULL, nrows=4800000)
+  tweets <- data.table::fread("tweets.csv", na.strings="NA", colClasses=NULL, nrows=480)
   tweets <- cbind(timestamp=as.POSIXct(paste (tweets$tweet_time, tweets$tweet_date, sep = " ") , format="%H:%M:%S %Y-%m-%d"), tweets)
   tweets <- cbind(date=as.POSIXct(tweets$tweet_date, format="%Y-%m-%d"), tweets)
   tweets$tweet_time <- NULL
   tweets$tweet_date <- NULL
   tweets
 }
-
+#----------------------------------------------------------------
+remove_urls <- function(tweets)
+{
+  tweets$content <- gsub("?(f|ht)tp(s?)://\\S+\\s*", "", tweets$content)
+  tweets
+}
+#----------------------------------------------------------------
+remove_usernames <- function(tweets)
+{
+  tweets$content <- gsub("?(@)\\S+\\s*", "", tweets$content)
+  tweets
+}
+#----------------------------------------------------------------
+remove_hash_tags <- function(tweets)
+{
+  tweets$content <- gsub("?(#)\\S+\\s*", "", tweets$content)
+  tweets
+}
+#----------------------------------------------------------------
+remove_hash_tag_sign <- function(tweets)
+{
+  tweets$content <- gsub("?(#)", "", tweets$content)
+  tweets
+}
+#----------------------------------------------------------------
+remove_numbers <- function(tweets)
+{
+  tweets$content <- gsub("[[:digit:]]", "", tweets$content)
+  tweets
+}
+#----------------------------------------------------------------
+detect_tweets_language <- function(tweets) {
+  clean.tweets <- remove_urls(tweets)
+  clean.tweets <- remove_usernames(clean.tweets)
+  clean.tweets <- remove_hash_tags(clean.tweets)
+  clean.tweets <- remove_numbers(clean.tweets)
+  
+  tweets.language = textcat(clean.tweets$content)
+  tweets <- cbind(language=tweets.language , tweets)
+  tweets
+}
+#----------------------------------------------------------------
 load_keywords <- function() {
   keywords <- read.csv(file="strings", header=FALSE)
   names(keywords) <- c("keyword")
   keywords
 }
-
+#----------------------------------------------------------------
 prepare_histogram <- function(tweets, keyword) {
   #number of tweets per hour
   #hist(tweets$timestamp, length(tweets$timestamp), breaks = 5040, freq = TRUE, xlab = "Date", ylab = "Tweets Frequency", main = paste("Keyword:'",keyword,"'"))
@@ -48,8 +92,7 @@ prepare_histogram <- function(tweets, keyword) {
                  labels=date_format("%b %d %y") )
   base
 }
-
-
+#----------------------------------------------------------------
 find_tweet_frequency <- function(tweets) {
 
   tweets <- cbind(as.POSIXct(paste (tweets$tweet_time, tweets$tweet_date, sep = " ") , format="%H:%M:%S %Y-%m-%d"), tweets)
@@ -58,7 +101,7 @@ find_tweet_frequency <- function(tweets) {
   tweets$tweet_date <- NULL
   tweets
 }
-
+#----------------------------------------------------------------
 filter_dataset_by_keyword <- function( tweets, keyword )
 {
   # Replace space with ".*" to build a regex expression that will search for all keywords
@@ -66,17 +109,50 @@ filter_dataset_by_keyword <- function( tweets, keyword )
   search.result <- with(tweets , str_detect(tweets$content, keyword.regex))
   tweets[search.result,drop=TRUE]
 }
+#----------------------------------------------------------------
+#----------------------------------------------------------------
+#----------------------------------------------------------------
+#----------------------------------------------------------------
 
-test <- function(tweets, test.keyword)
-{
-  printf("Current keyword: %s", test.keyword)
-}
 
-#sapply(X = keywords$keyword , FUN = filter_dataset_by_keyword, tweets = tweets)
+#--------------- Main Code -------------------
+
+
+#-------------- Loading Data -----------------
 
 printf("Loading dataset, it might take a while ...\n")
 tweets <- load_dataset() 
 printf("Dataset sucessfully loaded.\n")
+
+#-------------- Text Cleaning -----------------
+
+tweets <- remove_urls(tweets)
+tweets <- remove_usernames(tweets)
+tweets <- remove_hash_tags(tweets)
+tweets <- remove_numbers(tweets)
+#------------- Creating Corpus-----------------
+tweets.corpus <- Corpus(VectorSource(tweets$content))
+# convert to lowercase
+tweets.corpus <- tm_map(tweets.corpus, tolower)
+# remove punctuation
+tweets.corpus <- tm_map(tweets.corpus, removePunctuation)
+# remove numbers
+tweets.corpus <- tm_map(tweets.corpus, removeNumbers)
+# stem words
+tweets.corpus <- tm_map(tweets.corpus, stemDocument)
+
+
+
+
+#----------- Langugae Detection ---------------
+tweets <- detect_tweets_language(tweets)
+View(tweets)
+#------- Frequent Words and Associations ------
+
+
+#--------------- Word Cloud -------------------
+
+#--------------- Clustering -------------------
 
 printf("Loading keywords ...\n")
 keywords <- load_keywords()
@@ -88,6 +164,8 @@ for(i in 1:length(keywords$keyword)){
   printf("Preparing dataset %d base on '%s' keyword\n",i , keywords$keyword[i])
   segmented.tweets.list[[i]] <- filter_dataset_by_keyword( tweets, keywords$keyword[i] )
 }
+
+#----------- Cluster Histogram ---------------
 
 histogram.list = list()
 for(i in 1:length(keywords$keyword)){
@@ -105,4 +183,10 @@ for(i in 1:length(keywords$keyword)){
     printf("Histogram %d is empty\n",i)
   }
 }
+#------------- Topic Modeling -----------------
+
+View(tweets)
+
+
+#sapply(X = keywords$keyword , FUN = filter_dataset_by_keyword, tweets = tweets)
 
